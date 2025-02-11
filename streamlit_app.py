6c3757bec291118a5b141d578afd134ac141b96a
LucijaBrcic <lbrcic612@gmail.com>
2025-02-11 11:24:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('🎈 App Name')
 
-pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
 
 st.write('Hello world!')
